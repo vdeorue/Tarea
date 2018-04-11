a6a1c80aa628c9bf23f5d84c11d162ae203a7db9
vdeorue <v.deorue01@ufromail.cl>
2018-04-11 11:07:20 -0300

    Tarea

--- src/tarea/Tarea.java
+++ src/tarea/Tarea.java
@@ -8,6 +8,7 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.*;
 /**
  *
  * @author Vicente
@@ -20,12 +21,15 @@ public class Tarea {
    }
    public static void loadArray(String csvFile){
     //Codigo para leer archivos csv.
+    ArrayList<Estudiante> Lista = new ArrayList<Estudiante>() ;
+    Estudiante Est;
     BufferedReader br = null;
     String line = "";
     //Se define separador ","
     String cvsSplitBy = ";";
     try {
         br = new BufferedReader(new FileReader(csvFile));
+        br.readLine();
         while ((line = br.readLine()) != null) {                
             String[] datos = line.split(cvsSplitBy);
             //Imprime datos.
@@ -33,8 +37,10 @@ public class Tarea {
             if(rutchar[8] == 'K'){
                 rutchar[8]= '0';
                 datos[1]=String.valueOf(rutchar);
+
             }
-            
+            /*Est = new Estudiante(Integer.parseInt(datos[1]),datos[0],datos[2]);
+            Lista.add(Est);*/// Codigo para agregar los datos a un arraylist de la clase estudiante (prueba).
         System.out.println(datos[0] + ", " + datos[1] + ", " + datos[2]);
         }
     } catch (FileNotFoundException e) {
