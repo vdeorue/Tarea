1d3edcbc02335f773f15354293b06fdc6a388d68
ajneb97 <benjamin_ld@hotmail.com>
2018-04-13 12:04:25 -0300

    Update Tarea.java

--- src/tarea/Tarea.java
+++ src/tarea/Tarea.java
@@ -16,45 +16,68 @@ import java.util.*;
 public class Tarea {
 
 
-   public static void main(String[] args) throws IOException {
-    loadArray("estudiantes.csv");
-   }
-   public static void loadArray(String csvFile){
-    //Codigo para leer archivos csv.
-    ArrayList<Estudiante> Lista = new ArrayList<Estudiante>() ;
-    Estudiante Est;
-    BufferedReader br = null;
-    String line = "";
-    //Se define separador ","
-    String cvsSplitBy = ";";
-    try {
-        br = new BufferedReader(new FileReader(csvFile));
-        br.readLine();
-        while ((line = br.readLine()) != null) {                
-            String[] datos = line.split(cvsSplitBy);
-            //Imprime datos.
-            char[] rutchar= datos[1].toCharArray();
-            if(rutchar[8] == 'K'){
-                rutchar[8]= '0';
-                datos[1]=String.valueOf(rutchar);
+   private static ArrayList<Estudiante> estudiantes;
+	public static void main(String[] args) throws IOException {
+		estudiantes = new ArrayList<Estudiante>();
+	    loadArray("estudiantes.csv");
+	    
+	    /*
+	    for(int i=0;i<estudiantes.size();i++) {
+	    	estudiantes.get(i).display();
+	    }
+	    */
+	    ordenarEstudiantes();
+	    
+	}
+	
+	private static void ordenarEstudiantes() {
+		QuickSort q = new QuickSort(estudiantes.size());
+		for(int i=0;i<estudiantes.size();i++) {
+			q.insert(estudiantes.get(i));
+		}
+		System.out.println("-----DESORDENADO-----");
+		q.display();
+		q.quickSort();
+		System.out.println("-----ORDENADO-----");
+		q.display();
+		
+	}
 
-            }
-            /*Est = new Estudiante(Integer.parseInt(datos[1]),datos[0],datos[2]);
-            Lista.add(Est);*/// Codigo para agregar los datos a un arraylist de la clase estudiante (prueba).
-        System.out.println(datos[0] + ", " + datos[1] + ", " + datos[2]);
-        }
-    } catch (FileNotFoundException e) {
-        e.printStackTrace();
-    } catch (IOException e) {
-        e.printStackTrace();
-    } finally {
-        if (br != null) {
-            try {
-                br.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-    }
-   }
+	public static void loadArray(String csvFile){
+	    //Codigo para leer archivos csv.
+	    BufferedReader br = null;
+	    String line = "";
+	    //Se define separador ","
+	    String cvsSplitBy = ";";
+	    try {
+	        br = new BufferedReader(new FileReader(csvFile));
+	        br.readLine();
+	        while ((line = br.readLine()) != null) {                
+	            String[] datos = line.split(cvsSplitBy);
+	            //Imprime datos.
+	            char[] rutchar= datos[1].toCharArray();
+	            /*
+	            if(rutchar[8] == 'K'){
+	                rutchar[8]= '0';
+	                datos[1]=String.valueOf(rutchar);
+
+	            }
+	            */
+	            Estudiante Est = new Estudiante(datos[1],datos[0],datos[2]);
+	            estudiantes.add(Est); // Codigo para agregar los datos a un arraylist de la clase estudiante (prueba).
+	        }
+	    } catch (FileNotFoundException e) {
+	        e.printStackTrace();
+	    } catch (IOException e) {
+	        e.printStackTrace();
+	    } finally {
+	        if (br != null) {
+	            try {
+	                br.close();
+	            } catch (IOException e) {
+	                e.printStackTrace();
+	            }
+	        }
+	    }
+	}
 }
